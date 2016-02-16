4eb4d54e689b925505e4ed061f34384807919ac4
ohs <ohs@bouvet.no>
2016-02-16 15:31:05 +0100

    Added Metadata handling, tested PoC

--- src/main/java/no/eatools/util/ImageMetadata.java
+++ src/main/java/no/eatools/util/ImageMetadata.java
@@ -23,9 +23,13 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import no.eatools.diagramgen.ImageFileFormat;
+
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 
 /**
@@ -33,18 +37,26 @@ import org.w3c.dom.Node;
  */
 public class ImageMetadata {
     private static final transient Logger LOG = LoggerFactory.getLogger(ImageMetadata.class);
+//    public static final String TEXT_NODE_NAME = "Text";
+//    public static final String TEXT_ENTRY_NODE_NAME = "TextEntry";
+
+    public static final String TEXT_NODE_NAME = "tEXt";
+    public static final String TEXT_ENTRY_NODE_NAME = "tEXtEntry";
+
+    public static final String KEYWORD = "keyword";
+    public static final String VALUE = "value";
+    public static final String DEFAULT_FORMAT_NAME = "javax_imageio_png_1.0";
+
 
     public static void main(final String[] args) {
         final ImageMetadata meta = new ImageMetadata();
-        final int length = args.length;
-        for (int i = 0; i < length; i++)
-            meta.readAndDisplayMetadata(args[i]);
+        for (final String arg : args) {
+            meta.readAndDisplayMetadata(arg);
+        }
     }
 
-
-    ImageReader openImageFile(final String fileName) {
+    ImageReader openImageFile(final File file) {
         try {
-            final File file = new File(fileName);
             final ImageInputStream iis = ImageIO.createImageInputStream(file);
             final Iterator<ImageReader> readers = ImageIO.getImageReaders(iis);
 
@@ -65,44 +77,65 @@ public class ImageMetadata {
     }
 
     void readAndDisplayMetadata(final String fileName) {
+        readAndDisplayMetadata(new File(fileName));
+    }
+
+    void readAndDisplayMetadata(final File file) {
         try {
-            final File file = new File(fileName);
-            final ImageInputStream iis = ImageIO.createImageInputStream(file);
-            final Iterator<ImageReader> readers = ImageIO.getImageReaders(iis);
-
-            if (readers.hasNext()) {
-
-                // pick the first available ImageReader
-                final ImageReader reader = readers.next();
-
-                // attach source to the reader
-                reader.setInput(iis, true);
-                final BufferedImage bufferedImage = reader.read(0);
+            final ImageReader reader = openImageFile(file);
+//                final BufferedImage bufferedImage = reader.read(0);
 
 //                writeCustomData(bufferedImage, "Hei", "Haa");
 
-                // read metadata of first image
-                final IIOMetadata metadata = reader.getImageMetadata(0);
-
-                final String[] names = metadata.getMetadataFormatNames();
-                for (final String name : names) {
-                    System.out.println("Format name: " + name);
-                    final Node asTree = metadata.getAsTree(name);
-                    displayMetadata(asTree);
+            // read metadata of first image
+            final IIOMetadata metadata = reader.getImageMetadata(0);
+            LOG.debug("Native format name: {}", metadata.getNativeMetadataFormatName());
+            final String[] names = metadata.getMetadataFormatNames();
+            for (final String name : names) {
+                LOG.debug("Format name: {}", name);
+                final Node asTree = metadata.getAsTree(name);
+                displayMetadata(asTree);
 ////                    final IIOMetadataNode child = new IIOMetadataNode("Ove");
 ////                    child.setAttribute("now", new Date().toString());
 //                    asTree.appendChild(child);
 //                    displayMetadata(asTree);
-                }
+//                displayMetadata(findTextNode(asTree));
             }
+
         } catch (final Exception e) {
             LOG.error("Error ", e);
         }
     }
 
-    public byte[] writeCustomData(final BufferedImage buffImg, final String key, final String value) throws Exception {
-        final ImageWriter writer = ImageIO.getImageWritersByFormatName("png")
-                                          .next();
+    Node findTextNode(final IIOMetadata metadata) {
+        metadata.getNativeMetadataFormatName();
+        for (final String formatName : metadata.getMetadataFormatNames()) {
+            final Node asTree = metadata.getAsTree(formatName);
+            final Node textNode = findTextNode(asTree);
+            if(textNode != null) {
+                return textNode;
+            }
+        }
+        return null;
+    }
+
+    Node findTextNode(final Node root) {
+        final NodeList children = root.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            final Node child = children.item(i);
+            if (TEXT_NODE_NAME.equalsIgnoreCase(child.getNodeName())) {
+                return child;
+            }
+            final Node candidate = findTextNode(child);
+            if(candidate != null) {
+                return candidate;
+            }
+        }
+        return null;
+    }
+
+    public byte[] writeCustomData(final BufferedImage buffImg, final File file, final String key, final String value) throws Exception {
+        final ImageWriter writer = ImageIO.getImageWritersByFormatName(ImageFileFormat.PNG.toString().toLowerCase()).next();
 
         final ImageWriteParam writeParam = writer.getDefaultWriteParam();
         final ImageTypeSpecifier typeSpecifier = ImageTypeSpecifier.createFromBufferedImageType(BufferedImage.TYPE_INT_RGB);
@@ -110,22 +143,30 @@ public class ImageMetadata {
         //adding metadata
         final IIOMetadata metadata = writer.getDefaultImageMetadata(typeSpecifier, writeParam);
 
-        final IIOMetadataNode textEntry = new IIOMetadataNode("tEXtEntry");
-        textEntry.setAttribute("keyword", key);
-        textEntry.setAttribute("value", value);
+        Node textNode = findTextNode(metadata);
+        if(textNode == null) {
+            textNode = new IIOMetadataNode(TEXT_NODE_NAME);
+//            final Node root = metadata.getAsTree(metadata.getNativeMetadataFormatName());
+//            root.appendChild(textNode);
+        } else {
+            LOG.debug("---------------- TExt node found ");
+        }
 
-        final IIOMetadataNode text = new IIOMetadataNode("tEXt");
-        text.appendChild(textEntry);
+        final IIOMetadataNode textEntry = new IIOMetadataNode(TEXT_ENTRY_NODE_NAME);
+        textEntry.setAttribute(KEYWORD, key);
+        textEntry.setAttribute(VALUE, value);
+
+        textNode.appendChild(textEntry);
 
 //        metadata.mergeTree();
-        final IIOMetadataNode root = new IIOMetadataNode("javax_imageio_png_1.0");
-        root.appendChild(text);
+        final IIOMetadataNode root = new IIOMetadataNode(DEFAULT_FORMAT_NAME);
+        root.appendChild(textNode);
 
-        metadata.mergeTree("javax_imageio_png_1.0", root);
+        metadata.mergeTree(DEFAULT_FORMAT_NAME, root);
 
         //writing the data
         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        final FileOutputStream fios = new FileOutputStream("xx.png");
+        final FileOutputStream fios = new FileOutputStream(file);
         final ImageOutputStream stream = ImageIO.createImageOutputStream(fios);
         writer.setOutput(stream);
         writer.write(metadata, new IIOImage(buffImg, null, metadata), writeParam);
@@ -137,37 +178,6 @@ public class ImageMetadata {
 
         return baos.toByteArray();
     }
-
-//    void addMetadata(final String fileName, String key, String value) {
-//        try {
-//
-//            final File file = new File(fileName);
-//            final ImageOutputStream ios = ImageIO.createImageOutputStream(file);
-//            final Iterator<ImageWriter> readers = ImageIO.getImageWriters();
-//
-//            if (readers.hasNext()) {
-//                // pick the first available ImageReader
-//                final ImageReader reader = readers.next();
-//
-//                // attach source to the reader
-//                reader.setInput(iis, true);
-//
-//                // read metadata of first image
-//                final IIOMetadata metadata = reader.getImageMetadata(0);
-//
-//                final String[] names = metadata.getMetadataFormatNames();
-//                for (String name : names) {
-//                    System.out.println("Format name: " + name);
-//                    Node asTree = metadata.getAsTree(name);
-//                    asTree.appendChild(new IIOMetadataNode("Ove"));
-//                    asTree.
-//                                  displayMetadata(asTree);
-//                }
-//            }
-//        } catch (final Exception e) {
-//            LOG.error("Error ", e);
-//        }
-//    }
 
     void displayMetadata(final Node root) {
         System.out.println(formatXML(root));
@@ -191,4 +201,25 @@ public class ImageMetadata {
         }
     }
 
+    public void writeCustomMetaData(final File file, final String key, final String value) {
+        LOG.debug("Opening file {}", file.getAbsolutePath());
+        LOG.debug("*** Meta before modification ***");
+        readAndDisplayMetadata(file);
+        LOG.debug("*** / ***");
+
+        final ImageReader reader = openImageFile(file);
+        try {
+            final File tmpFile = File.createTempFile("metaTmp", ImageFileFormat.PNG.getFileExtension(), file.getParentFile());
+            tmpFile.deleteOnExit();
+            LOG.debug("Created tmp file {}", tmpFile.getAbsolutePath());
+            writeCustomData(reader.read(0), tmpFile, key, value);
+            LOG.debug("*** Meta after modification ***");
+            readAndDisplayMetadata(tmpFile);
+            LOG.debug("*** / ***");
+            file.delete();
+            FileUtils.copyFile(tmpFile, file);
+        } catch (final Exception e) {
+            LOG.error("Error on creating metadata", e);
+        }
+    }
 }
